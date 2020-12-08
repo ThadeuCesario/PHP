3eda751e4e99b5d8da179ec6d7c6173c286724d0
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 17:48:22 -0300

    17:48:22 - Javascript

--- JS-functional/functions/callback_2.js
+++ JS-functional/functions/callback_2.js
@@ -4,7 +4,7 @@
  * Também utilizaremos o path, para captura o caminho de um arquivo.
  * 
  * O join() serve para concatenar caminhos, porque sabemos que para cada sistema operacional pode variar
- * um pouco, principalmente 
+ * um pouco, principalmente o sentido das barras.
  */
 
 const fs = require('fs');
