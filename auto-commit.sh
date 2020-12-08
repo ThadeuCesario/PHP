a7e96a59ffe710e6dac214ad2d9262a949dfb97f
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 11:54:28 -0300

    11:54:28 - Javascript - Autocommit

--- auto-commit.sh
+++ auto-commit.sh
@@ -4,7 +4,7 @@ do
   sleep 2 &
   now=$(date +"%T")
   git add .
-  git commit -m "$now - Javascript - Autocommit"
+  git commit -m "$now - Javascript - Desafio"
   git push
   echo "Autocommit finished."
   wait
