534d00db0d85c7e9cb60ac61339fb06a539711c2
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 11:46:42 -0300

    11:46:42 - Javascript - Autocommit

--- JS-functional/functions/basico_2.js
+++ JS-functional/functions/basico_2.js
@@ -24,3 +24,9 @@ executarQualquerCoisa(3);
 
 
 // Retornando uma função a partir de uma outra função.
+function potencia(base, exp){
+  return Math.pow(base, exp);
+}
+
+const bits8 = potencia(2, 8);
+console.log(bits8);
