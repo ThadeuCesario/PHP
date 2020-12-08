beafe97f7f4803cf3b862189b50a26c9511dbdfd
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 20:31:15 -0300

    20:31:15 - Javascript

--- JS-functional/functions/callback_3.js
+++ JS-functional/functions/callback_3.js
@@ -18,5 +18,5 @@ const carrinho = [
   { nome: 'Impressora', qtde: 0, preco: 7.99 },
   { nome: 'Caderno', qtde: 4, preco: 7.99 },
   { nome: 'Lapis', qtde: 3, preco: 7.99 },
-  { nome: 'Tesoura', qtde: 10, preco: 7.99 },
+  { nome: 'Tesoura', qtde: 1, preco: 7.99 },
 ]
