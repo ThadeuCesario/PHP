c6fd9a5c03798b7ecc5fe10f54d2383d2c26d416
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 20:14:42 -0300

    20:14:42 - Javascript

--- JS-functional/functions/callback_3.js
+++ JS-functional/functions/callback_3.js
@@ -10,4 +10,5 @@ const dobro = (n, i, a) => n * 2 + i + a.length;
 console.log(nums.map(dobro));
 
 const nomes = ['Ana', 'Bia', 'Gui', 'Lia', 'Rafa'];
-const primeiroLetra = name => name.charAt(0)
+const primeiroLetra = name => name.charAt(0);
+console.log()
