336d4986c66d6daf34c8bb4a3608e5086663565e
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 17:51:24 -0300

    17:51:24 - Javascript

--- JS-functional/functions/callback_2.js
+++ JS-functional/functions/callback_2.js
@@ -13,5 +13,5 @@ const path = require('path');
 
 const caminho = path.join(__dirname, 'dados.txt');
 
-const callback = () => console.log('Leitura finalizada');
+const callback = () => console.time('Leitura finalizada');
 fs.readFile(caminho, callback);
