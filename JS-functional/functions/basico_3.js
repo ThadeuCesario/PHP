e7c01c744802667124d99605d2fbfe03480242c3
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 14:31:22 -0300

    14:31:22 - Javascript - Functions

--- JS-functional/functions/basico_3.js
+++ JS-functional/functions/basico_3.js
@@ -5,10 +5,11 @@ felizNatal();
 const saudacao = (nome) => `Fala ${nome}!!`;
 console.log(saudacao('Thadeu')); 
 
-const somar = array => {
+const somar = numeros => {
   let tota = 0;
-  for (let i of array) {
+  for (let i of numeros) {
     tota += i;
   }
   return tota;
 }
+somar([1,2,3,5]);
