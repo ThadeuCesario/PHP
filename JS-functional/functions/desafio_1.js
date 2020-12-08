4995d1d64a3f86cedb7a535fdf640a90bfabc604
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 13:48:01 -0300

    13:48:01 - Javascript - Desafio

--- JS-functional/functions/desafio_1.js
+++ JS-functional/functions/desafio_1.js
@@ -14,3 +14,8 @@ console.log(somar(3)(4)(5));
 // fn -> 3 + 7
 // fn -> 3 - 7
 // calcular(3)(7)(fn)
+
+const somar = (a, b) => a + b;
+const multiplicar = (a, b) => a * b;
+const subtrair = (a, b) => a - b;
+const dividir = (a, b) => a / b;
