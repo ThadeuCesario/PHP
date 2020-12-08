86934172b6a8838fcbab4ab5881b8e20fd1ce277
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 20:06:37 -0300

    20:06:37 - Javascript

--- JS-functional/functions/callback_3.js
+++ JS-functional/functions/callback_3.js
@@ -1,3 +1,10 @@
 const nums = [1, 2, 3, 4, 5];
+
+/**
+ * 
+ * @param {*} n 
+ * @param {*} i 
+ * @param {*} a 
+ */
 const dobro = (n, i, a) => n * 2 + i + a.length;
 console.log(nums.map(dobro));
