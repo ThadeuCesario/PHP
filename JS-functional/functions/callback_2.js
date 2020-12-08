4158f726e76cc32d9d0bbfc92bb8869874623aae
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 17:46:05 -0300

    17:46:05 - Javascript

--- JS-functional/functions/callback_2.js
+++ JS-functional/functions/callback_2.js
@@ -1,6 +1,7 @@
 /**
  * Estaremos utilizando uma biblioteca do próprio Node chamado FileSystem, para realizarmos a leitura de
  * um arquivo.
+ * Também utilizaremos o path, para captura o caminho de um arquivo.
  */
 
 const fs = require('fs');
