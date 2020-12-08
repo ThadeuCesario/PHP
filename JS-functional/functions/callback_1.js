9be3bce2fbd37e001c86ef4fd874214094e2c00b
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 17:33:13 -0300

    17:33:13 - Javascript

--- JS-functional/functions/callback_1.js
+++ JS-functional/functions/callback_1.js
@@ -4,7 +4,7 @@ function exec(fn, a, b){
 
 
 const somarNoTerminal = (a, b) => a + b;
-const subtrairNoTerminal = (a, b) = a - b;
+const subtrairNoTerminal = (a, b) => a - b;
 
 exec(somarNoTerminal, 56, 38);
 exec(subtrairNoTerminal, 182, 27);
