939ada02306d7962e534cd499e50109b50b4f2eb
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 20:07:22 -0300

    20:07:22 - Javascript

--- JS-functional/functions/callback_3.js
+++ JS-functional/functions/callback_3.js
@@ -3,7 +3,8 @@ const nums = [1, 2, 3, 4, 5];
 /**
  * Um map possui três parametros. 
  * Primeiro: Elemento atual
- * Segundo: índice 
+ * Segundo: índice atual
+ * 
  */
 const dobro = (n, i, a) => n * 2 + i + a.length;
 console.log(nums.map(dobro));
