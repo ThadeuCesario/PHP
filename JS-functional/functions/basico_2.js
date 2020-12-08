4608df2eec11b907a2d70f769c15691cd7a346a9
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 11:31:26 -0300

    11:31:26 - Javascript - Autocommit

--- JS-functional/functions/basico_2.js
+++ JS-functional/functions/basico_2.js
@@ -1,3 +1,11 @@
+function bomDia(){
+  console.log('Bom dia!');
+}
+
+function boaTarde(){
+  console.log('Boa tarde!');
+}
+
 function executarQualquerCoisa(fn){
   console.log(typeof fn);
   fn();
