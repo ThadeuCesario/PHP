d48860a2e7168244d0794cbc6145f14c8c9c69d7
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-09 18:38:25 -0300

    18:38:25 - Javascript

--- JS-functional/functions/desafio_2.js
+++ JS-functional/functions/desafio_2.js
@@ -3,5 +3,5 @@ const carrinho = [
   {nome: 'Impressora', qtde: 0, preco: 649.50, fragil: true},
   {nome: 'Caderno', qtde: 4, preco: 27.10, fragil: false},
   {nome: 'Lapis', qtde: 3, preco: 5.82, fragil: false},
-  {nome: 'Tesoura', qtde: 1, preco: 19.20},
+  {nome: 'Tesoura', qtde: 1, preco: 19.20, fragil: true},
 ];
