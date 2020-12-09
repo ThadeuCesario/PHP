d7e479e0eb039e9d294a02dea9756e6eaf95a625
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-09 20:10:14 -0300

    20:10:14 - Javascript

--- JS-functional/functions/desafio_2.js
+++ JS-functional/functions/desafio_2.js
@@ -14,6 +14,6 @@ const isFragile = product => product.fragil;
 const getTotal = product => product.preco * product.qtde;
 
 const media = carrinho.filter(isFragile).map(getTotal).reduce((acc, el) => {
-  const novaQtde = acc
+  const novaQtde = acc.qtde
 });
 console.log(media);
