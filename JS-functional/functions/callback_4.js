1d4c02cdc0ee9d023c263d69f980a7f09171756b
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-09 14:23:58 -0300

    14:23:58 - Javascript

--- JS-functional/functions/callback_4.js
+++ JS-functional/functions/callback_4.js
@@ -1,7 +1,7 @@
 const carrinho = [
   {nome: 'Caneta', qtde: 10, preco: 7.99},
-  {nome: 'Impressora', qtde: 0, preco: },
-  {nome: 'Caderno', qtde: 4, preco: 7.99},
+  {nome: 'Impressora', qtde: 0, preco: 649.50},
+  {nome: 'Caderno', qtde: 4, preco: },
   {nome: 'Lapis', qtde: 3, preco: 7.99},
   {nome: 'Tesoura', qtde: 1, preco: 7.99},
 ]
