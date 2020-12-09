e801fb4292601e950f96582f4553988f08759506
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-09 18:57:28 -0300

    18:57:28 - Javascript

--- JS-functional/functions/desafio_2.js
+++ JS-functional/functions/desafio_2.js
@@ -18,7 +18,10 @@ console.log(allTotal);
 
 // 3. Média dos totais
 const avarage = allTotal.reduce((accumulator, element, index) => {
-  console.log(allTotal.length, index);
+  console.log("log", allTotal.length, index);
+  if(index === allTotal.length) {
+    console.log("ultimo elemento");
+  } 
   return accumulator + element;
 }) 
 
