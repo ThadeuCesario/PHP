d352e9873c53de3307120fe0b655740191dea9ba
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-09 14:33:22 -0300

    14:33:22 - Javascript

--- JS-functional/functions/callback_4.js
+++ JS-functional/functions/callback_4.js
@@ -7,7 +7,7 @@ const carrinho = [
 ];
 
 const qtdeMaiorQueZero = item => item.qtde > 0;
-const qtdeMaiorIgual
+const qtdeMaiorIgualAZero = item => item
 const qtdeMuitoGrande = item => item.qtde >= 1000;
 const getNome = item => item.nome;
 
