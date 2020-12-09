e9d08468b1eb66f90e69af64a137476a406bca00
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-09 14:47:42 -0300

    14:47:42 - Javascript

--- JS-functional/functions/callback_4.js
+++ JS-functional/functions/callback_4.js
@@ -32,6 +32,6 @@ Array.prototype.meuFilter = function(fn) {
 }
 
 const maiorQueQuarenta = valor => valor > 40;
-const maiorQueDezoito
+const maiorQueDezoito = valor => valor > 18;
 const arrayTeste = [5, 10, 15, 23, 89, 1256];
 console.log(arrayTeste.meuFilter(maiorQueQuarenta));
