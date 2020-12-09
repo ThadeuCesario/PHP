c0240fd0c9f5f3a396cef57bca018e51ba8c44e0
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-09 18:45:31 -0300

    18:45:31 - Javascript

--- JS-functional/functions/desafio_2.js
+++ JS-functional/functions/desafio_2.js
@@ -8,7 +8,8 @@ const carrinho = [
 
 // 1. Retornar elementos que são frágeis.
 const isFragile = product => product.fragil;
-const allFragile = carrinho.filter()
+const allFragile = carrinho.filter(isFragile);
+console.log(allFragile);
 
 
 // 2. Retornar a quantidade e o preço de cada produto. -> Precisamos gerar o total de cada elemento
