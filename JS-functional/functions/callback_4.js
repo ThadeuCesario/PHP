6168d33dec0bada6c13368e6d003009aa84f8897
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-09 14:45:38 -0300

    14:45:38 - Javascript

--- JS-functional/functions/callback_4.js
+++ JS-functional/functions/callback_4.js
@@ -23,7 +23,7 @@ console.log(produtosEmEstoque);
 Array.prototype.meuFilter = function(fn) {
   const filtered = [];
   for(let i = 0; i < this.length; i++) {
-    if(fn(this[i])) filtered.push(this[])
+    if(fn(this[i])) filtered.push(this[i]);
   }
 }
 
