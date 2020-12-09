9818cb33a5eb2d1e72a4921b7ab04f8ea7bb43ab
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-09 18:27:41 -0300

    18:27:41 - Javascript

--- JS-functional/functions/callback_5.js
+++ JS-functional/functions/callback_5.js
@@ -23,3 +23,11 @@
  * Mas isso ocorre somente quando não temos um valor inicial passado para o reduce.
  * 
  */
+
+const carrinho = [
+  {nome: 'Caneta', qtde: 1000, preco: 7.99},
+  {nome: 'Impressora', qtde: 0, preco: 649.50},
+  {nome: 'Caderno', qtde: 4, preco: 27.10},
+  {nome: 'Lapis', qtde: 3, preco: 5.82},
+  {nome: 'Tesoura', qtde: 1, preco: 19.20},
+];
