d2b84f8570f5cd24e835cd42a94780d302315fe2
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-08 21:46:38 -0300

    21:46:38 - Javascript

--- JS-functional/functions/callback_3.js
+++ JS-functional/functions/callback_3.js
@@ -28,4 +28,4 @@ const precoConsolidado = carrinho.map(produto => produto.qtde * produto.preco);
 console.log(nomeProdutos);
 console.log(precoConsolidado);
 
-Array.prototype.
+Array.prototype.meuMap
