31a5e7d45495c86e9ab74ac0e5cd5cc34284f163
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-09 15:19:39 -0300

    15:19:39 - Javascript

--- JS-functional/functions/callback_5.js
+++ JS-functional/functions/callback_5.js
@@ -4,4 +4,6 @@
  * -> array.reduce(funcao, valor_inicial)
  * No primeiro parâmetro, estamos passando uma função e no segundo parâmetro um valor inicial que será utilizado
  * pelo próprio reduce para trabalhar com o restante do array.
+ * 
+ * 
  */
