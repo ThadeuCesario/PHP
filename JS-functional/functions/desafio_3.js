86bfea4adf1969e983ef602a2faecb9f952d6bbc
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-11 17:01:29 -0300

    17:01:29 - Javascript

--- JS-functional/functions/desafio_3.js
+++ JS-functional/functions/desafio_3.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 function readFile(caminho) {
   return new Promise(function(resolve){
-    fs.readFile(caminho, {}, resolve(caminho)) 
+    fs.readFile(caminho, {}, ) 
   })
 }
 
