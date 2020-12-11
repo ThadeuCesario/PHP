4bc7406b7ef25ad25a556b440594852dace2627b
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-11 14:46:33 -0300

    14:46:33 - Javascript

--- JS-functional/functions/promise_1.js
+++ JS-functional/functions/promise_1.js
@@ -22,6 +22,7 @@
 
 p.then(valor => valor[0])
  .then(primeiro => primeiro[0])
- .then(letra => letra.toLowerCase());
+ .then(letra => letra.toLowerCase())
+ .then(letraMinuscula => console.log(letraMinuscula));
  
 console.log(typeof Promise);
