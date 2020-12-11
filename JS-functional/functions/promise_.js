70311de2b63cec87413c5a43729d87ec4c243d4d
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-11 15:18:11 -0300

    15:18:11 - Javascript

--- JS-functional/functions/promise_.js
+++ JS-functional/functions/promise_.js
@@ -2,6 +2,8 @@ setTimeout(function() {
   console.log('Execuando callback......');
   setTimeout(function() {
     console.log('Execuando callback......');
-    
+    setTimeout(function(){
+      
+    })
   }, 2000)
 }, 2000);
