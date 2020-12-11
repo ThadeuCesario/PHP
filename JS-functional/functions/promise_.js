b1eb8a05c0848ebe8ab55943b17d859c5aa00524
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-11 15:23:20 -0300

    15:23:20 - Javascript

--- JS-functional/functions/promise_.js
+++ JS-functional/functions/promise_.js
@@ -17,7 +17,6 @@ function esperarPor(tempo = 2000) {
   return new Promise(function(resolve){
     setTimeout(function(){
       console.log('Executando promise...');
-      resolve('Teste'); 
     }, tempo);
   });
 }
