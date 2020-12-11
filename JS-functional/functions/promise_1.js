e0dafdbebf372c0d49d17798ab95e4bee5192617
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-11 14:47:11 -0300

    14:47:11 - Javascript

--- JS-functional/functions/promise_1.js
+++ JS-functional/functions/promise_1.js
@@ -22,6 +22,9 @@
    cumprirPromessa(['Thadeu', 'Karina', 'Daisy', 'Thalyta']);
  });
 
+function primeiroElemento() {
+  
+}
 p.then(valor => valor[0])
  .then(primeiro => primeiro[0])
  .then(letra => letra.toLowerCase())
