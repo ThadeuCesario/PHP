85948f81e15d0fe24e8306ff3ef3f856c2f67613
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-11 15:48:36 -0300

    15:48:36 - Javascript

--- JS-functional/functions/promise_3.js
+++ JS-functional/functions/promise_3.js
@@ -2,7 +2,7 @@ function gerarNumerosEntre(min, max){
   if(min > max) [min, max] = [max, min];
  
   return new Promise(function(resolve){
-    const aleatorio = Math.random * (max - min) 
+    const aleatorio = Math.random * (max - min) + min
     resolve(7)
   })
 }
