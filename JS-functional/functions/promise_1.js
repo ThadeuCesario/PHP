57972d5f7256dfeb1c35b06a2f12e10fef353f8c
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-11 14:49:43 -0300

    14:49:43 - Javascript

--- JS-functional/functions/promise_1.js
+++ JS-functional/functions/promise_1.js
@@ -34,7 +34,7 @@ function converterParaLetraMinuscula(letra){
   return letra.toLowerCase();
 }
 
-function imprimirResultado()
+function imprimirResultado(valor)
 
 p.then(primeiroElemento)
  .then(primeiraLetra)
