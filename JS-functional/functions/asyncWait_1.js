196a3e3700e0822c598e0d4623270ee684583575
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-13 14:02:31 -0300

    14:02:31 - Javascript

--- JS-functional/functions/asyncWait_1.js
+++ JS-functional/functions/asyncWait_1.js
@@ -6,7 +6,6 @@
 function esperarPor(tempo = 2000) {
   return new Promise(function(resolve){
     setTimeout(() => {
-      console.log('Executando promise...');
       resolve();
     }, tempo);
   });
