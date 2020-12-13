5feb90cb9da713c1ee3c561520372c4d80ea3267
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-13 15:56:37 -0300

    15:56:37 - Javascript

--- JS-functional/functions/asyncWait_2.js
+++ JS-functional/functions/asyncWait_2.js
@@ -1,53 +1,9 @@
-/**
- * Async e Await
- * Basicamente o async await força o aguardar a finalização de um determinado trecho de código.
- * Sempre que temos uma função que retorna um Promise, podemos aplicar o await.
- * 
- * Quando marcamos uma função com async, temos a possibilidade de aguardar que uma promise seja executada dentro dela,
- * sem necessariamente utilizar o método then. 
- */
-
-function esperarPor(tempo = 2000) {
+function gerarNumerosEntre(min, max, tempo = 2000){
+  if(min > max) [min, max] = [max, min];
+ 
   return new Promise(function(resolve){
-    setTimeout(() => resolve(), tempo);
-  });
+    setTimeout(function(){
+      resolve(parseInt(Math.random() * (max - min + 1)) + min);
+    }, tempo);
+  })
 }
-
-esperarPor(2000)
-  .then(() => console.log('Executando promise 1.....'))
-  .then(esperarPor)
-  .then(() => console.log('Executando promise 2.....'))
-  .then(esperarPor)
-  .then(() => console.log('Executando promise 3.....'));
-
-  function retornarValor() {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve(10)
-      }, 5000);
-    });
-  }
-
-
-async function executar() {
-  let valor = await retornarValor();
-
-  await esperarPor(1500);
-  console.log(`Async/Await ${valor}...`);
-
-  await esperarPor(1500);
-  console.log(`Async/Await ${valor + 1}...`);
-
-  await esperarPor(1500);
-  console.log(`Async/Await ${valor + 2}...`);
-
-  return valor + 3;
-}
-
-async function getValue() {
-  const valor = await executar();
-  console.log(valor);
-}
-
-getValue();
-
