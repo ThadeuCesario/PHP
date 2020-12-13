bbe000be17689682162f7a52ad7629ece3396392
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-13 15:57:46 -0300

    15:57:46 - Javascript

--- JS-functional/functions/asyncWait_2.js
+++ JS-functional/functions/asyncWait_2.js
@@ -3,7 +3,7 @@ function gerarNumerosEntre(min, max, numeroProibidos){
  
   return new Promise(function(resolve){
     setTimeout(function(){
-      resolve(parseInt(Math.random() * (max - min + 1)) + min);
+      
     });
   })
 }
