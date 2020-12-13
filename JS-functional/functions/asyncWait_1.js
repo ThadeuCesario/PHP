9cedae5926f005137d99da84ac2acc45515bd3eb
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-13 14:12:34 -0300

    14:12:34 - Javascript

--- JS-functional/functions/asyncWait_1.js
+++ JS-functional/functions/asyncWait_1.js
@@ -21,9 +21,10 @@ esperarPor(2000)
 async function executar() {
   esperarPor(2000);
   console.log('Async/Await 1...');
-  
+
   esperarPor(2000);
   console.log('Async/Await 2...');
+  
   esperarPor(2000);
   console.log('Async/Await 3...');
 }
