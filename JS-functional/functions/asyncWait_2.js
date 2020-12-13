f9f0a7add95ee54ff9c9cf8c20ba1c25bd54258a
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-13 15:59:33 -0300

    15:59:33 - Javascript

--- JS-functional/functions/asyncWait_2.js
+++ JS-functional/functions/asyncWait_2.js
@@ -1,6 +1,5 @@
 function gerarNumerosEntre(min, max, numeroProibidos){
   if(min > max) [min, max] = [max, min];
- 
   return new Promise((resolve, reject) => {
     setTimeout(function(){
       const fator = max - min + 1;
