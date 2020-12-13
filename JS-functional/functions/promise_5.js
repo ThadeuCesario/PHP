c9d8eba4fb323cca37c52abf8937ee03b2e22f17
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-13 11:41:45 -0300

    11:41:45 - Javascript

--- JS-functional/functions/promise_5.js
+++ JS-functional/functions/promise_5.js
@@ -1,6 +1,6 @@
 /**
  * Para uma promisse podemos passar uma função de resolve e uma função de reject.
- * A função de resolve é executada quando a promisse é chamada e executado corretamente. Mas caso ocorra algum erro, 
+ * A função de resolve é executada quando a promisse é chamada e executado corretamente. Mas caso ocorra algum erro, a função de reject 
  */
 
 function funcionarOuNao(valor, chanceErro) {
