363d84208a9394060640262a7141273e66e2a7d3
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-14 13:54:20 -0300

    13:54:20 - Javascript

--- JS-functional/functions/OO_2.js
+++ JS-functional/functions/OO_2.js
@@ -9,3 +9,14 @@ class Produto {
     return this.preco * (1 - desc);
   }
 }
+
+const p1 = new Produto('Caneta', 1.50);
+const p2 = new Produto('Geladeira', 2000.00);
+
+console.log(p1);
+console.log(`Verificando nome: ${p1.nome}`);
+console.log(`Verificando preço com desconto: ${p1.precoFinal()}`); 
+
+console.log(p2);
+console.log(`Verificando preço: ${p2.preco}`);
+console.log(`Verificando preço com desconto: ${p2.precoFinal()}`);
