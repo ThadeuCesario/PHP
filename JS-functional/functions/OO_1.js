c99e6aea02c38adc3683d36ee95ff77540441ae0
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-14 13:11:17 -0300

    13:11:17 - Javascript

--- JS-functional/functions/OO_1.js
+++ JS-functional/functions/OO_1.js
@@ -13,7 +13,7 @@ const p1 = new Produto('Caneta', 1.50);
 const p2 = new Produto('Geladeira', 2000.00);
 
 console.log(p1);
-console.log(p1.nome);
+console.log();
 
 console.log(p2);
 console.log(p2.preco);
