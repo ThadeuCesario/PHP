cb8797c2853a5cce76c41744513b2add3e442033
Thadeu Munhoz Cesario <thadeu.cesario@icloud.com>
2020-12-14 14:11:21 -0300

    14:11:21 - Javascript

--- JS-functional/functions/OO_3.js
+++ JS-functional/functions/OO_3.js
@@ -25,7 +25,7 @@ class Produto {
  * Utilizando o prototype podemos criar uma função que não foi definida dentro da função construtora.
  */
 Produto.prototype.log = function() {
-  console.log(`Nome: ${this.nome} Preço: R$${this.preco}`);
+  return console.log(`Nome: ${this.nome} Preço: R$${this.preco}`);
 }
 
 const p1 = new Produto('Caneta', 1.50);
